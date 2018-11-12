bc713360e7ea2412116785aaa84326dcd978e21d
Salvatore Ravidà <salvoravida@gmail.com>
2018-11-13 00:22:06 +0100

    setState support callback => setState(value,callback)

--- src/core/useClassStateKey.js
+++ src/core/useClassStateKey.js
@@ -2,6 +2,7 @@
  *  https://github.com/salvoravida/react-class-hooks
  */
 
+import invariant from 'tiny-invariant';
 import { getMagicSelf, checkSymbol, MAGIC_STATES } from './magicSelf';
 
 export function useClassStateKey(keySymbol, initialValue) {
@@ -16,11 +17,12 @@ export function useClassStateKey(keySymbol, initialValue) {
     if (!self[MAGIC_STATES].hasOwnProperty(keySymbol)) {
         self[MAGIC_STATES][keySymbol] = {
             value: typeof initialValue === 'function' ? initialValue() : initialValue,
-            setValue: (value) => {
+            setValue: (value, callback) => {
                 self[MAGIC_STATES][keySymbol].value = typeof value === 'function'
                     ? value(self[MAGIC_STATES][keySymbol].value) : value;
                 //check if mounted yet
-                if (self.updater.isMounted(self)) self.forceUpdate();
+                invariant(!callback || typeof callback === 'function', 'setState callback must be a function!');
+                if (self.updater.isMounted(self)) self.forceUpdate(callback);
             },
         };
     }
